453b6d7406b340df04b9e9a0f020161f1b661590
tomasep22 <tomas.ep22@gmail.com>
2021-04-06 00:48:30 -0300

    Added form with inputs

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const books = library.querySelectorAll('.book');
 
 let myLibrary = ['the hobbit', 'YDKJS', 'The principles of OOP'];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(book) {
   // do stuff here
-return myLibrary.push(book)
+myLibrary.push(book)
+const newBook = document.createElement('div');
+newBook.classList.add('book');
+newBook.textContent = book.title;
+library.appendChild(newBook);
 }
 
 function displayBooks() {
@@ -22,3 +30,16 @@ function displayBooks() {
 }
 
 displayBooks();
+
+document.newBook.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const title = this.title.value;
+  const author = this.author.value;
+  const pages = this.pages.value;
+  const read = this.read.checked ? 'Read' : 'Not read yet'
+  console.log(title, author, pages, read);
+  let obj = new Book(title, author, pages, read);
+  console.log(obj);
+  addBookToLibrary(obj);
+  this.reset();
+})
